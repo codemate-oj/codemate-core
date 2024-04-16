1a8d92918744800de79deb5d03508b5c8be57d73
StupidQu <104199438+StupidQu@users.noreply.github.com>
2024-04-16 23:28:31 +0800

    fix: removed unused imports

--- packages/codemate-plugin/plugins/sms-register/index.ts
+++ packages/codemate-plugin/plugins/sms-register/index.ts
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid';
 import {
     BlacklistedError, BlackListModel,
-    Context, Handler, param, PERM, PRIV, SettingModel, superagent, SystemModel, TokenModel, Types, UserAlreadyExistError, UserModel,
+    Context, Handler, param, PERM, PRIV, SettingModel, superagent, SystemModel, TokenModel, Types, UserModel,
 } from 'hydrooj';
 import { logger } from './lib';
 
