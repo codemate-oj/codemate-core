b1f391e9e11998552224169e93c52a8a71946224
StupidQu <104199438+StupidQu@users.noreply.github.com>
2024-05-20 00:18:54 +0800

    fix: ???

--- packages/utils/lib/locate-pm2.ts
+++ packages/utils/lib/locate-pm2.ts
@@ -1,17 +1,8 @@
-import path from 'path';
-
 const PATH = process.env.PATH?.split(':') || [];
 
 // @ts-ignore
 const pm2: typeof import('pm2') | null = (() => {
-    for (const dir of PATH) {
-        try {
-            const p = path.resolve(dir.split('.bin')[0], 'pm2');
-            if (require.resolve(p)) return require(p); // eslint-disable-line import/no-dynamic-require
-        } catch (e) {
-        }
-    }
-    return null;
+    return require('pm2');
 })();
 
 export default pm2;
