6e688e8edb8100722af97f6b1caa6201e7967055
StupidQu <104199438+StupidQu@users.noreply.github.com>
2024-05-20 00:13:42 +0800

    fix: this will work, I think

--- packages/utils/lib/locate-pm2.ts
+++ packages/utils/lib/locate-pm2.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import path from 'path';
 
 const PATH = process.env.PATH?.split(':') || [];
@@ -7,16 +6,9 @@ const PATH = process.env.PATH?.split(':') || [];
 const pm2: typeof import('pm2') | null = (() => {
     for (const dir of PATH) {
         try {
-            let info;
-            try {
-                info = fs.readlinkSync(path.resolve(dir.split('.bin')[0], 'pm2'));
-            } catch {
-                info = dir;
-            }
-            const p = path.resolve(dir, info);
-            return require(`${p.split('.bin')[0]}pm2`); // eslint-disable-line import/no-dynamic-require
+            const p = path.resolve(dir.split('.bin')[0], 'pm2');
+            return require(p); // eslint-disable-line import/no-dynamic-require
         } catch (e) {
-            console.log(e);
         }
     }
     return null;
