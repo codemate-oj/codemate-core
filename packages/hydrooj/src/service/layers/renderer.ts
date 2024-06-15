9a9eff920c39dc8a604f70ef671a30f85599aa75
KiritoKing <kiritoclzh@gmail.com>
2024-06-15 16:19:41 +0000

    chore: remove url prefix

--- packages/hydrooj/src/service/layers/renderer.ts
+++ packages/hydrooj/src/service/layers/renderer.ts
@@ -61,8 +61,6 @@ export default (router, logger) => async (ctx: KoaContext, next) => {
             res = router.url(name, args, { query }).toString();
             if (anchor) res = `${res}#${anchor}`;
             if (withDomainId) res = `/d/${withDomainId}${res}`;
-            const urlPrefix = system.get('server.url');
-            if (urlPrefix && urlPrefix !== '/') res = urlPrefix + res;
         } catch (e) {
             logger.warn(e.message);
             logger.info('%s %o', name, args);
