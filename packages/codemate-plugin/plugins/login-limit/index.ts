2eb4077148160a05e024d7b0add313a1035c72b4
StupidQu <104199438+StupidQu@users.noreply.github.com>
2024-07-03 07:33:07 +0800

    fix: use inject

--- packages/codemate-plugin/plugins/login-limit/index.ts
+++ packages/codemate-plugin/plugins/login-limit/index.ts
@@ -9,24 +9,26 @@ declare module '../kv-service' {
 }
 
 export async function apply(ctx: Context) {
-    const callback = async (that: Handler & Record<string, any>) => {
-        if (that.session.uid <= 1) return; // user login failed
-        const udoc = await UserModel.getById(that.args['domainId'], that.session.uid);
-        const userIp = that.request.ip;
-        const ipWhiteList = await ctx.kv.get('ipWhiteList');
-        if (ipWhiteList.includes(userIp)) return;
-        if (udoc.hasPriv(PRIV.PRIV_UNLIMITED_ACCESS)) return;
-        const tdocs = await TokenModel.getMulti(TokenModel.TYPE_SESSION, { uid: udoc._id }).toArray();
-        await Promise.all(tdocs.map(async (tdoc) => TokenModel.del(tdoc._id, TokenModel.TYPE_SESSION)));
-    };
-    ctx.on('handler/after/TOTPLogin', callback);
-    ctx.on('handler/after/UserLogin', callback);
+    ctx.inject(['kv'], async (c) => {
+        const callback = async (that: Handler & Record<string, any>) => {
+            if (that.session.uid <= 1) return; // user login failed
+            const udoc = await UserModel.getById(that.args['domainId'], that.session.uid);
+            const userIp = that.request.ip;
+            const ipWhiteList = await c.kv.get('ipWhiteList');
+            if (ipWhiteList.includes(userIp)) return;
+            if (udoc.hasPriv(PRIV.PRIV_UNLIMITED_ACCESS)) return;
+            const tdocs = await TokenModel.getMulti(TokenModel.TYPE_SESSION, { uid: udoc._id }).toArray();
+            await Promise.all(tdocs.map(async (tdoc) => TokenModel.del(tdoc._id, TokenModel.TYPE_SESSION)));
+        };
+        c.on('handler/after/TOTPLogin', callback);
+        c.on('handler/after/UserLogin', callback);
 
-    // add local ips to whitelist
-    await (async () => {
-        const ipWhiteList = (await ctx.kv.get('ipWhiteList')) || [];
-        if (ipWhiteList.length === 0) {
-            await ctx.kv.set('ipWhiteList', ['127.0.0.1', '::1', '::ffff:127.0.0.1']);
-        }
-    })();
+        // add local ips to whitelist
+        await (async () => {
+            const ipWhiteList = (await c.kv.get('ipWhiteList')) || [];
+            if (ipWhiteList.length === 0) {
+                await c.kv.set('ipWhiteList', ['127.0.0.1', '::1', '::ffff:127.0.0.1']);
+            }
+        })();
+    });
 }
