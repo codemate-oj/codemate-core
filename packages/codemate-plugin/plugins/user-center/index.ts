4f9d44a2dc9a7bc9a46b435c114b511f645ccd1b
StupidQu <1165282370@qq.com>
2024-07-26 19:23:51 +0800

    feat: migrate user-center

---
+++ packages/codemate-plugin/plugins/user-center/index.ts
@@ -0,0 +1,39 @@
+import { Context, Handler, param, post, TokenModel, Types, Udoc, UserModel, UserNotFoundError } from 'hydrooj';
+
+class UserCenterHandler extends Handler {
+    @param('tabId', Types.Int, true)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async get(domainId: string, tabId: number = 1) {
+        const uid = this.user._id;
+        if (uid === 0) throw new UserNotFoundError(0);
+        const [udoc, sdoc] = await Promise.all([
+            UserModel.getById(domainId, uid),
+            TokenModel.getMostRecentSessionByUid(uid, ['createAt', 'updateAt']),
+        ]);
+        if (!udoc) throw new UserNotFoundError(uid);
+        this.response.body = {
+            udoc,
+            sdoc,
+        };
+    }
+
+    @post('nickname', Types.String, true)
+    @post('nationality', Types.String, true)
+    @post('age', Types.Int, true)
+    @post('oier', Types.Boolean, true)
+    async post(domainId: string, nickname: string = '', nationality: string = '中国', age: number = 0, oier: boolean = false) {
+        const uid = this.user._id;
+        if (uid === 0) throw new UserNotFoundError(0);
+        const $update: Partial<Udoc> = {
+            nationality,
+            age,
+            oier,
+            nickname,
+        };
+        await UserModel.setById(uid, $update);
+    }
+}
+
+export async function apply(ctx: Context) {
+    ctx.Route('user_center', '/user/center', UserCenterHandler);
+}
