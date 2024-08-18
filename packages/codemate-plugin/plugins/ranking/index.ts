c2784e55fa0f814b5f1ce4a1ae395fa48a471748
KiritoKing <kiritoclzh@gmail.com>
2024-08-18 13:17:41 +0000

    fix: enhance ranking api

--- packages/codemate-plugin/plugins/ranking/index.ts
+++ packages/codemate-plugin/plugins/ranking/index.ts
@@ -3,13 +3,14 @@ import { BadRequestError, Context, DomainModel as domain, Handler, paginate, PER
 class DomainRankHandler extends Handler {
     @query('page', Types.PositiveInt, true)
     @query('rankBy', Types.String, true)
-    async get(domainId: string, page = 1, rankBy: string = 'all') {
+    @query('pageSize', Types.PositiveInt, true)
+    async get(domainId: string, page = 1, rankBy: string = 'all', pageSize = 100) {
         if (!['all', 'Py', 'Cpp', 'Scratch'].includes(rankBy)) throw new BadRequestError();
         const cursor =
             rankBy === 'all'
                 ? domain.getMultiUserInDomain(domainId, { uid: { $gt: 1 }, rp: { $gt: 0 } }).sort({ rp: -1 })
                 : domain.getMultiUserInDomain(domainId, { uid: { $gt: 1 }, rp: { $gt: 0 } }).sort({ [`rp${rankBy}`]: -1 });
-        const [dudocs, upcount, ucount] = await paginate(cursor, page, 100);
+        const [dudocs, upcount, ucount] = await paginate(cursor, page, pageSize);
         const udict = await user.getList(
             domainId,
             dudocs.map((dudoc) => dudoc.uid),
