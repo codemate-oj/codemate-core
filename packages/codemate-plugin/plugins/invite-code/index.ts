5f3b730786a09930db353b2f56067faa95893c39
KiritoKing <kiritoclzh@gmail.com>
2024-08-26 14:40:30 +0000

    feat: enable batch create invite codes

--- packages/codemate-plugin/plugins/invite-code/index.ts
+++ packages/codemate-plugin/plugins/invite-code/index.ts
@@ -1,9 +1,10 @@
-import { Context, Handler, PERM } from 'hydrooj';
+import { Context, Handler, param, PERM, Types } from 'hydrooj';
 import { InvitatonModel } from './model';
 
 class InvitationCreateHandler extends Handler {
-    async post() {
-        const code = await InvitatonModel.add(this.domain._id, this.user._id);
+    @param('num', Types.PositiveInt, true)
+    async post(domainId: string, num = 1) {
+        const code = await Promise.all(Array.from({ length: num }, () => InvitatonModel.add(this.domain._id, this.user._id)));
         this.response.body = {
             success: true,
             code,
