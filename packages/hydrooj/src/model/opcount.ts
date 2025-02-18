2016e03f8a6407976098c28141a66fcbb5e16cb3
poc <poc@classcv.com>
2025-02-18 12:03:45 +0800

    fix: occasional frequency misjudgment

--- packages/hydrooj/src/model/opcount.ts
+++ packages/hydrooj/src/model/opcount.ts
@@ -6,28 +6,32 @@ const coll = db.collection('opcount');
 export async function inc(op: string, ident: string, periodSecs: number, maxOperations: number) {
     const now = new Date().getTime();
     const expireAt = new Date(now - (now % (periodSecs * 1000)) + periodSecs * 1000);
-    try {
-        const res = await coll.findOneAndUpdate(
-            {
-                op,
-                ident,
-                expireAt,
-                opcount: { $lt: maxOperations },
-            },
-            { $inc: { opcount: 1 } },
-            { upsert: true, returnDocument: 'after' },
-        );
-        return res.value.opcount;
-    } catch (e) {
-        if (e.message.includes('duplicate')) throw new OpcountExceededError(op, periodSecs, maxOperations);
-        throw e;
+    const count = await coll.countDocuments({
+        op,
+        ident,
+        expireAt,
+        opcount: { $gte: maxOperations },
+    });
+    if (count > 0) {
+        throw new OpcountExceededError(op, periodSecs, maxOperations);
     }
+    const res = await coll.updateOne(
+        {
+            op,
+            ident,
+            expireAt,
+            opcount: { $lt: maxOperations },
+        },
+        { $inc: { opcount: 1 } },
+        { upsert: true },
+    );
+    return res.modifiedCount;
 }
 
 export const apply = () =>
     db.ensureIndexes(
         coll,
         { key: { expireAt: -1 }, name: 'expire', expireAfterSeconds: 0 },
-        { key: { op: 1, ident: 1, expireAt: 1 }, name: 'unique', unique: true },
+        { key: { op: 1, ident: 1, expireAt: 1 }, name: 'optimisticLocking' },
     );
 global.Hydro.model.opcount = { inc, apply };
